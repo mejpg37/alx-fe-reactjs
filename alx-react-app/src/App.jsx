1989693caad3a49fbb87fad664de3c9eebf439b1
mejpg37 <131238440+mejpg37@users.noreply.github.com>
2025-10-29 11:13:01 +0300

    feat(task1): add WelcomeMessage component and integrate into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <WelcomeMessage />
-    </>
+    </div>
   );
 }
 
